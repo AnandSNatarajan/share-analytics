7dd49aca4bc34a9190c58452ad1b3eca0a6450a9
Anand Natarajan <anandsnatarajan@gmail.com>
2017-09-28 12:05:51 +0530

    Modified based on class.

--- strategies.py
+++ strategies.py
@@ -1,100 +1,47 @@
-###############################################################################
-# Given a particular share name, get the share price and volume information for
-# all dates specified in the "dates" file and store them into an array. This 
-# array can then be manipulated to get analytics information.
-###############################################################################
-
 import sys
-import subprocess
-import json
-import re
-import numpy as np
+import stockramdb as srb
 
-# Initialize the dates, closing price and volume arrays.
 dates = []
-cp = []
-volume = []
-
+stocklist = []
 total_success = 0
 total_failure = 0
-
-def sma_calculator(values, window):
-    weights = np.repeat(1.0,window)/window
-    smas = np.convolve(values, weights, 'valid')
-    return smas
-
-def parse_info(scrip):
-
-    del dates[:]
-    del cp[:]
-    del volume[:]
-
-    # Open the dates file.
-    f = open("dates", "r")
-
-    for line in f:
-
-        # Get a date from the file.
-        line = line.rstrip('\n')
-
-        # Construct the command for querying share data for a particular date.
-        sub_cmd = "ovsdb-client transact --pretty '[\"Share_List\",{\"op\":\"select\", \"table\":\"Share_List\", \"where\":[[\"name\",\"==\",\""+scrip+"\"],[\"date\",\"==\","+line+"]], \"columns\":[\"name\",\"date\",\"cp\",\"volume\"]}]'"
-        out = subprocess.check_output(sub_cmd, shell=True)
-
-        # Create a list with the JSON information.
-        raw_data = json.loads(out)
-
-        # Append the data to date array.
-        dates.append(int(line))
-    
-        # Append the data to the closing price array.
-        m1 = re.search('cp\': u\'([0-9\.]*)', str(raw_data[0]))
-        cp.append(float(m1.group(1)))
-            
-        # Append the data to the volume array.
-        m2 = re.search('volume\': ([0-9]*)', str(raw_data[0]))
-        volume.append(int(m2.group(1)))
-
-def dump_stock_data():
-    for i in range (0, len(dates) - 1):
-        print(str(dates[i]) + " " + str(cp[i]) + " " + str(volume[i]))
 
 # In this strategy, we look at a volume increase of above 50% of previous day and a price drop. This means there is off-loading
 # but at the same time there is an investment interest. So the stock can potentially go up very soon.
 
-def strategy_1 (scrip):
+def strategy_1 (s):
     success = 0
     failure = 0
+    prev_volume = s.volume[0]
+    prev_price = s.cp[0]
+    global total_success
+    global total_failure
 
-    # Calculate SMAs (it results in an array that is less in size of window_size - 1 because the first elements
-    # cannot have an SMA. So this needs to be adjusted while looking up for trigger.
-    window_size = 10
-    smas = sma_calculator(cp, window_size)
+    for day in range (1, len(s.dates) - 1):
 
-    for day in range (0, len(dates) - 1):
-        if (day > 0):
-            if ((volume[day] > prev_volume) & (((volume[day] - prev_volume) * 100)/prev_volume > 50) \
-                    & (cp[day] < prev_price)):
-                #print("Volume and price increased on " + str(dates[day]))
-                if ((cp[day] < cp[day+1])):
-                    success += 1 
-                    print("Success date : " + str(dates[day]))
-                        #print("Price increased next day" + str(cp[day]) + " " + str(cp[day+1]))
-                else:
-                    failure += 1 
-                    print("Failure date : " + str(dates[day]))
-                    #print("Price decreased next day " + str(cp[day]) + " " + str(cp[day+1]))
-            prev_volume = volume[day]
-        prev_price = cp[day]
+        if ((s.volume[day] > prev_volume) & (((s.volume[day] - prev_volume) * 100)/prev_volume > 50) & (s.cp[day] < prev_price)):
 
-    success_percentile = (success * 100)/(success + failure)
-    print("%s Success percentile : %d%% Sucess %d Failure %d" (scrip, success_percentile, success, failure))
+            if ((s.cp[day] < s.cp[day+1])):
+                success += 1 
+                print("Success date : " + str(s.dates[day]))
+            else:
+                failure += 1 
+                print("Failure date : " + str(s.dates[day]))
+
+            prev_volume = s.volume[day]
+            prev_price = s.cp[day]
+
+    if (success + failure > 0):
+        success_percentile = (success * 100)/(success + failure)
+        print("%s Success percentile : %d%% Sucess %d Failure %d" % (s.name, success_percentile, success, failure))
+    else:
+        print("%s No hits" % scrip)
 
 
 # In this strategy, we look at stocks that have gone down for 3 consecutive days. If the price of the stock went up after the 4th day
 # in any of the next 3 days, the strategy is considered as a success.
 
-def strategy_2 (scrip):
+def strategy_2 (s):
     success = 0
     failure = 0
     down_day = 0
@@ -102,37 +49,57 @@ def strategy_2 (scrip):
     global total_failure
 
     # Check if the price is dropping for 3rd consecutive day and check price on 4th day.
-    for day in range (1, len(dates) - 1):
-        if (cp[day-1] > cp[day]):
+    for day in range (1, len(s.dates) - 1):
+
+        if (s.cp[day-1] > s.cp[day]):
             down_day += 1
         else:
             down_day = 0
 
         if (down_day == 3):
+
             down_day = 0
-            if ((day+6) < (len(cp) - 1)): 
-                if ((cp[day+4] > cp[day]) | (cp[day+5] > cp[day]) | (cp[day+6] > cp[day])):
+
+            if ((day+6) < (len(s.cp) - 1)): 
+                if ((s.cp[day+4] > s.cp[day]) | (s.cp[day+5] > s.cp[day]) | (s.cp[day+6] > s.cp[day])):
                     success += 1
                     total_success += 1
-                    #print("Success date : %s" % dates[day])
+                    print("Success date : %s" % dates[day])
                 else:
                     failure += 1
                     total_failure += 1
-                    #print("Failure date : %s" % dates[day])
+                    print("Failure date : %s" % dates[day])
 
-    success_percentile = (success * 100)/(success + failure)
-    print("%s Success percentile : %d%% Sucess %d Failure %d" % (scrip, success_percentile, success, failure))
+    if (success + failure > 0):
+        success_percentile = (success * 100)/(success + failure)
+        print("%s Success percentile : %d%% Sucess %d Failure %d" % (s.name, success_percentile, success, failure))
+    else:
+        print("%s No hits" % scrip)
 
-# Open the stocks file.
+# Open the dates file.
 f = open(sys.argv[1], "r")
 
 for line in f:
+
     # Get a date from the file.
     line = line.rstrip('\n')
 
-    parse_info(line)
+    dates.append(line)
 
-    # Execute the strategy of choice
-    strategy_3(line)
+# Open the stocks file.
+f = open(sys.argv[2], "r")
 
-print("Total success : %d failure : %d" % (total_success, total_failure))
+for line in f:
+    # Get a stock name from the file.
+    line = line.rstrip('\n')
+
+    s = srb.stockdata(line)
+
+    s = srb.parse_stocks(s, dates)
+
+    stocklist.append(s)
+
+for s in stocklist:
+    strategy_2(s)
+
+print("Total success : %d, Total failure : %d" % (total_success, total_failure))
