0810f9bc3d56ce5d168a14d6883397a544521aee
AnandSNatarajan <anandsnatarajan@gmail.com>
2020-06-14 21:33:27 +0530

    Cleanup bhavcopy into a separate repository.

--- utils/getdata.go
+++ utils/getdata.go
@@ -1,67 +1,67 @@
 package utils
 
 import (
-    //"fmt"
-    "log"
-    "strconv"
-    "github.com/gomodule/redigo/redis"
+	//"fmt"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"strconv"
 )
 
 func PullAllStockData(stock string, dates []string) ([]float64, []float64, []float64, []float64, []int64) {
 
-    var sopen, shigh, slow, sclose []float64
-    var volume []int64
+	var sopen, shigh, slow, sclose []float64
+	var volume []int64
 
-    conn, err := redis.Dial("tcp", "localhost:6379")
-    if err != nil {
-        log.Fatal(err)
-    }
+	conn, err := redis.Dial("tcp", "localhost:6379", redis.DialDatabase(14))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer conn.Close()
+	defer conn.Close()
 
-    for _, date := range dates {
-        var price string
-        var err error
-        var f float64
-        var i int64
+	for _, date := range dates {
+		var price string
+		var err error
+		var f float64
+		var i int64
 
-        price, err = redis.String(conn.Do("HGET", stock+":close", date))
-        if err != nil {
-            log.Print("Unable to retrieve close for ", stock, date)
-        }
-        f,err = strconv.ParseFloat(price, 64)
-        sclose = append(sclose, f)
+		price, err = redis.String(conn.Do("HGET", stock+":close", date))
+		if err != nil {
+			log.Print("Unable to retrieve close for ", stock, date)
+		}
+		f, err = strconv.ParseFloat(price, 64)
+		sclose = append(sclose, f)
 
-        price, err = redis.String(conn.Do("HGET", stock+":open", date))
-        if err != nil {
-            log.Print("Unable to retrieve open for ", stock, date)
-        }
-        f,err = strconv.ParseFloat(price, 64)
-        sopen = append(sopen, f)
+		price, err = redis.String(conn.Do("HGET", stock+":open", date))
+		if err != nil {
+			log.Print("Unable to retrieve open for ", stock, date)
+		}
+		f, err = strconv.ParseFloat(price, 64)
+		sopen = append(sopen, f)
 
-        price, err = redis.String(conn.Do("HGET", stock+":low", date))
-        if err != nil {
-            log.Print("Unable to retrieve low for ", stock, date)
-        }
-        f,err = strconv.ParseFloat(price, 64)
-        slow = append(slow, f)
+		price, err = redis.String(conn.Do("HGET", stock+":low", date))
+		if err != nil {
+			log.Print("Unable to retrieve low for ", stock, date)
+		}
+		f, err = strconv.ParseFloat(price, 64)
+		slow = append(slow, f)
 
-        price, err = redis.String(conn.Do("HGET", stock+":high", date))
-        if err != nil {
-            log.Print("Unable to retrieve high for ", stock, date)
-        }
-        f,err = strconv.ParseFloat(price, 64)
-        shigh = append(shigh, f)
+		price, err = redis.String(conn.Do("HGET", stock+":high", date))
+		if err != nil {
+			log.Print("Unable to retrieve high for ", stock, date)
+		}
+		f, err = strconv.ParseFloat(price, 64)
+		shigh = append(shigh, f)
 
-        price, err = redis.String(conn.Do("HGET", stock+":volume", date))
-        if err != nil {
-            log.Print("Unable to retrieve volume for ", stock, date)
-        }
-        i,err = strconv.ParseInt(price, 10, 64)
-        volume = append(volume, i)
-    }
+		price, err = redis.String(conn.Do("HGET", stock+":volume", date))
+		if err != nil {
+			log.Print("Unable to retrieve volume for ", stock, date)
+		}
+		i, err = strconv.ParseInt(price, 10, 64)
+		volume = append(volume, i)
+	}
 
-    StockCommentary(conn, stock, sopen, shigh, slow, sclose, dates)
+	StockCommentary(conn, stock, sopen, shigh, slow, sclose, dates)
 
-    return sopen, shigh, slow, sclose, volume
+	return sopen, shigh, slow, sclose, volume
 }
--- utils/stockredis.go
+++ utils/stockredis.go
@@ -1,76 +1,75 @@
 package utils
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "bufio"
-    "strings"
-    "io/ioutil"
-    "github.com/gomodule/redigo/redis"
-
+	"bufio"
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 var verbose bool = false
 
-func WriteOHLC (conn redis.Conn, stock string, open string, high string, low string, sclose string, volume string, date string) {
+func WriteOHLC(conn redis.Conn, stock string, open string, high string, low string, sclose string, volume string, date string) {
 
-    _, err := conn.Do("HMSET", stock+":open", date, open)
-    if err != nil {
-        log.Fatal(err)
-    }
-    _, err = conn.Do("HMSET", stock+":high", date, high)
-    if err != nil {
-        log.Fatal(err)
-    }
-    _, err = conn.Do("HMSET", stock+":low", date, low)
-    if err != nil {
-        log.Fatal(err)
-    }
-    _, err = conn.Do("HMSET", stock+":close", date, sclose)
-    if err != nil {
-        log.Fatal(err)
-    }
-    _, err = conn.Do("HMSET", stock+":volume", date, volume)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err := conn.Do("HMSET", stock+":open", date, open)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Do("HMSET", stock+":high", date, high)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Do("HMSET", stock+":low", date, low)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Do("HMSET", stock+":close", date, sclose)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Do("HMSET", stock+":volume", date, volume)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func PushAllStockData (data_dir string) {
+func PushAllStockData(data_dir string) {
 
-    files, err := ioutil.ReadDir(data_dir)
-    if err != nil {
-        log.Fatal(err)
-    }
+	files, err := ioutil.ReadDir(data_dir)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    conn, err := redis.Dial("tcp", "localhost:6379")
-    if err != nil {
-        log.Fatal(err)
-    }
+	conn, err := redis.Dial("tcp", "localhost:6379", redis.DialDatabase(14))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer conn.Close()
+	defer conn.Close()
 
-    for _, f := range files {
-        fmt.Println(f.Name())
-        readFile, err := os.Open(data_dir+"/"+f.Name())
-        if (err != nil) {
-          log.Fatal(err)
-        }
+	for _, f := range files {
+		fmt.Println(f.Name())
+		readFile, err := os.Open(data_dir + "/" + f.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        scanner := bufio.NewScanner(readFile)
-        for (scanner.Scan()) {
+		scanner := bufio.NewScanner(readFile)
+		for scanner.Scan() {
 
-            s := strings.Split(scanner.Text(), ",")
-            if (len(s) <= 1) {
-                fmt.Println("ERROR: Received blank line")
-                continue
-            }
-            d := s[1]
-            date := d[0:4]+"-"+d[4:6]+"-"+d[6:8]
+			s := strings.Split(scanner.Text(), ",")
+			if len(s) <= 1 {
+				fmt.Println("ERROR: Received blank line")
+				continue
+			}
+			d := s[1]
+			date := d[0:4] + "-" + d[4:6] + "-" + d[6:8]
 
-            // Write OHLC data 
-            WriteOHLC(conn, s[0], s[2], s[3], s[4], s[5], s[6], date)
-        }
-    }
+			// Write OHLC data
+			WriteOHLC(conn, s[0], s[2], s[3], s[4], s[5], s[6], date)
+		}
+	}
 }
